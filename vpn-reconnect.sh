4c7cac104a7a03740f11ee1319b1dbbf4feb99ed
Paul P <paul.podolny@gmail.com>
2016-01-16 22:43:32 +0200

    minor addons/fixes
    added check wifi changes

--- vpn-reconnect.sh
+++ vpn-reconnect.sh
@@ -3,13 +3,19 @@
 set -e
 set -u
 
-conn_name='test'
+wifi_name='my-wifi'
+conn_name='my-VPN'
 
 while [ 1 ] ;do
-  status=$(nmcli con status id ${conn_name} |grep GENERAL.STATE|awk -F: '{print $NF}'|sed 's/ //g')
-  if [[ ${status} !=  "activated" ]] ;then
-     echo "`date` oh oh, disconnection detected, trying to reconnect..."
-     nmcli con up id ${conn_name}
+  if ! ping -c1 www.google.com &>/dev/null; then
+     echo "`date` oh no, ping failed - trying to bring WiFi back up..."
+     nmcli nm wifi on
+  else
+     status=$(nmcli con status id ${conn_name} |grep GENERAL.STATE|awk -F: '{print $NF}'|sed 's/ //g')
+     if [[ ${status} !=  "activated" ]] ;then
+        echo "`date` oh oh, VPN disconnection detected, trying to reconnect..."
+        nmcli con up id ${conn_name}
+     fi
   fi
   sleep 5 
 done
