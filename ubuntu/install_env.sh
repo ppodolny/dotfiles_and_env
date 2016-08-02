4fe4d3bf75d69ea1823d7e7e0702421133e1c925
Paul Podolny <ppodolny@users.noreply.github.com>
2016-08-01 18:38:18 -0700

    Update install_env.sh

--- ubuntu/install_env.sh
+++ ubuntu/install_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# this script bootstraps a new working environment
+# this script bootstraps a new working environment on ubuntu, tested on 14.04.x , 16.04.x
 
 set -e 
 set -u
@@ -12,7 +12,7 @@ sudo apt-get install -y sysstat strace ltrace htop nload nmap tmux rsync wget cu
 
 # dev stuff
 sudo apt-get install -y build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext ccze pv jq  \
-python-pip python-virtualenv git meld vim git-extras unzip pigz ccze pv ipython parallel xchat terminator 
+python-pipgit meld vim git-extras unzip pigz ccze pv ipython parallel xchat terminator 
 
 # multimedia etc
 sudo apt-get install -y vlc nicotine
@@ -27,8 +27,7 @@ sudo apt-get install -y oracle-java8-installer
 # sublime
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 && \
 sudo apt-get update && \
-sudo apt-get install -y sublime-text-installer 
-
+sudo apt-get install -y sublime-text-installer && \
 # pycharm
 sudo wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu $(lsb_release -sc)-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list' && \
@@ -37,19 +36,17 @@ sudo apt-get install pycharm -y
 
 # virtualbox
 sudo bash -c "echo 'deb http://download.virtualbox.org/virtualbox/debian xenial contrib' >> /etc/apt/sources.list.d/vbox.list"
-sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update && \
 sudo apt-get install virtualbox-5.0 -y
 
 # vagrant
-vagrant_version="1.8.1"
+vagrant_version="1.8.5"
 wget https://releases.hashicorp.com/vagrant/${vagrant_version}/vagrant_${vagrant_version}_x86_64.deb -P /tmp && \
 sudo dpkg -i /tmp/vagrant_${vagrant_version}_x86_64.deb && \rm /tmp/vagrant*.deb
 
 # pip stuff
 sudo pip install pip --upgrade
-sudo pip install awscli requests simplejson boto setuptools PyYAML ansible Jinja2 argparse click pylint virtualenvwrapper
+sudo pip install awscli virtualenv requests simplejson boto setuptools PyYAML ansible Jinja2 argparse click pylintv virtualenvwrapper.project
 
 # fzf addon for bash
 cd /tmp ; git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
