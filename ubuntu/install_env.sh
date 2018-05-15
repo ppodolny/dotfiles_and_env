46bdf3be117ee7daf31f5cd7fa1a377e5c2bf525
paul-opsguru <paul@opsguru.io>
2018-05-15 11:54:53 -0700

    Update install_env.sh
    added k8s autocompletion & TF

--- ubuntu/install_env.sh
+++ ubuntu/install_env.sh
@@ -12,8 +12,8 @@ sudo apt-get install -y sysstat strace ltrace htop nload nmap tmux rsync \
 wget curl clustershell sysdig apt-file tree
 
 # dev stuff
-sudo apt-get install -y build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext ccze pv jq  \
-python-pip git meld vim git-extras unzip pigz ccze pv ipython parallel xchat terminator 
+sudo apt-get install -y npm build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext ccze pv jq  \
+python-pip git meld vim git-extras unzip pigz ccze pv ipython parallel xchat terminator nodejs 
 
 # multimedia etc
 sudo apt-get install -y vlc nicotine calibre
@@ -47,6 +47,13 @@ vagrant_version="2.0.1"
 wget https://releases.hashicorp.com/vagrant/${vagrant_version}/vagrant_${vagrant_version}_x86_64.deb -P /tmp && \
 sudo dpkg -i /tmp/vagrant_${vagrant_version}_x86_64.deb && \rm /tmp/vagrant*.deb
 
+#tf
+tf_version="0.11.7"
+wget https://releases.hashicorp.com/terraform/${tf_version}/terraform_${tf_version}_linux_amd64.zip -P /tmp
+cd !$; unzip terraform*.zip
+sudo \rm /tmp/terraform*.zip 
+sudo mv /tmp/terraform /usr/local/bin/terraform
+
 # docker
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' && \
@@ -55,18 +62,23 @@ sudo apt-get install -y docker-engine && \
 sudo systemctl status docker && \
 sudo usermod -aG docker $(whoami)
 
-# k8s
+# k8s stuff
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
 
 # pip stuff
 sudo pip install pip --upgrade
 
-# install pip packages globally
-sudo pip install pylint flake8 autopep8 awscli pyenv \
+# install pip packages
+pip install pylint flake8 autopep8 awscli pyenv \
 requests simplejson boto3 setuptools PyYAML ansible \
 Jinja2 argparse click pylintv 
+ 
+# serverless framework
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install -g serverless
 
 # fzf-addon for bash
 cd /tmp ; git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -74,9 +86,6 @@ cd /tmp ; git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # ruby stuff
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-# externall projects
-mkdir -p ~/git/external
 
 # tmux-css
 git clone https://github.com/dennishafemann/tmux-cssh ~/git/external
